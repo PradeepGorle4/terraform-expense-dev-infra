f0156f8869b3abdb6858dbaf560f15364ed01e54
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-02-16 17:51:01 +0530

    Expense-infra-dev

--- 30-vpn/vpn.tf
+++ 30-vpn/vpn.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "open_vpn" {
   key_name = aws_key_pair.openvpnas.key_name
   vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
   subnet_id = local.public_subnet_id
+  user_data = file("user-data.sh")
 
   tags = merge(
     var.common_tags,
@@ -16,4 +17,8 @@ resource "aws_instance" "open_vpn" {
         Name = "${var.project_name}-${var.environment}-vpn"
     }
   )
+}
+
+output "vpn_id" {
+    value = aws_instance.open_vpn.public_ip
 }

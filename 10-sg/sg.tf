dc6fb6f42e65af0bc05a792d5be1963bff91c70b
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-02-17 20:02:56 +0530

    Expense-infra-dev

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -153,6 +153,15 @@ resource "aws_security_group_rule" "backend_vpn" {
     security_group_id = module.backend_sg.sg_id
 }
 
+resource "aws_security_group_rule" "backend_vpn_8080" {
+  type = "ingress"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    source_security_group_id = module.vpn_sg.sg_id
+    security_group_id = module.backend_sg.sg_id
+}
+
 resource "aws_security_group_rule" "mysql_backend" { # This means mysql accepting traffic from backend
     type = "ingress"
     from_port = 3306
@@ -161,4 +170,6 @@ resource "aws_security_group_rule" "mysql_backend" { # This means mysql acceptin
     source_security_group_id = module.backend_sg.sg_id
     security_group_id = module.mysql_sg.sg_id
 }
+
+
 

94b4aac14b389c564294404fdf970c296671521f
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-02-17 10:51:47 +0530

    terraform-expense-dev

--- 40-rds/locals.tf
+++ 40-rds/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  resource_name = "${var.project_name}-${var.environment}"
+  resource_name = "mysql-${var.project_name}-${var.environment}"
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value # Here, we are storing the mysql_sg_id value which is queried from data block
   database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
 }
